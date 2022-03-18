75ff096082acaed0d5fe45bd38cb7790f23fd509
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2022-03-18 19:49:45 +0000

    ficha quase feita

---
+++ FP04/main.c
@@ -0,0 +1,15 @@
+//main.c
+#include "soio.h"
+#include <unistd.h>
+int main(){
+ SOFILE * fp; char c;
+ fp = sofopen("test.txt","r");
+ write (1, "Primeiro Caracter ", 18);
+ c = sofgetc (fp);
+ write (1, &c, 1);
+ write (1, "\n", 1);
+ write (1, "Resto do ficheiro:\n", 19);
+ while ( (c = sofgetc(fp)) !=EOF )
+ write( 1, &c, 1 );
+ sofclose(fp);
+}
