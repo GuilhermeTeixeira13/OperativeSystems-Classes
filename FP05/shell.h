d9629cf222b4ffcd82af09309771875c22c9da9d
Guilherme Teixeira <75620226+GuilhermeTeixeira13@users.noreply.github.com>
2022-03-23 17:37:42 +0000

    f05

---
+++ FP05/shell.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <ctype.h>
+
+int parse (char *buf, char **args);
+
+void execute (int numargs, char **args);
+
+int builtin (char **args);
+
+void socp(char *fonte, char *destino);
+
+void ioCopy (int IN, int OUT);
+
+/* constantes que podem tornar uteis*/
+
+#define BG 0
+#define FG 1
